aa16d55403f5da2bf9d0373a745ca8f93855b5c1
Chrisna Youk <cyouk@myges.com>
2025-06-27 13:01:59 +0200

    [test1]

--- src/main/java/org/example/game/Game.java
+++ src/main/java/org/example/game/Game.java
@@ -1,4 +1,16 @@
 package org.example.game;
 
 public class Game {
+    private int[] rolls = new int[21];
+    private int currentRoll = 0;
+
+    public void roll(int pins) {
+        rolls[currentRoll++] = pins;
+    }
+
+    private int score;
+    public int score() {
+        score = 0;
+        return score;
+    }
 }
